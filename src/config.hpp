c20dd16c3e0c1383b67b8bdb9fd103f8c17c25ed
system64fumo <system64fumo@protonmail.com>
2024-06-19 19:15:58 +0300

    Cleanup
    Create the main window properly (Hopefully in a few commits there will be no need to use win-> stuff anymore.
    
    Re ordered the config, I should.. really get to work on adding that todo..

--- src/config.hpp
+++ src/config.hpp
@@ -1,10 +1,9 @@
+#pragma once
+
 /*
 	Default config.
 	Can be configured instead of using launch arguments.
-	Runtime configuration can be disabled by deleting #define RUNTIME_CONFIG
 */
-
-#define RUNTIME_CONFIG
 
 // Current								Default
 inline std::string position = "bottom";	// bottom
@@ -17,3 +16,8 @@ inline std::string margins = "0 0 0 0";	// 0 0 0 0
 inline int desired_timeout = 3;			// 3
 inline int transition_time = 250;		// 250
 inline std::string backlight_path = "";	// ""
+
+// Build time configuration				Description
+#define RUNTIME_CONFIG					// Allow the use of runtime arguments
+// TODO: add options to select what stuff should be monitored
+// eg: audio_in, audio_out, brightness, ect..
