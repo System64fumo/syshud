5974836642bc22a7aa0c29bde150511e48498e90
system64fumo <system64fumo@tuta.io>
2024-11-03 23:41:56 +0200

    (EXPERIMENTAL) Update config system
    Another day, Another experimental update that breaks everything for everyone all at once.
    If you're reading this (First of all thank you) the fix is to update the names in config.conf (Just copy a new config from /usr/share/sys64/hud/config.conf)
    
    Oh uh yeah, Sorry for the breakage!

--- src/main.hpp
+++ src/main.hpp
@@ -1,11 +1,11 @@
 #pragma once
-#include "window.hpp"
-
+#include <map>
+#include <string>
 #include <gtkmm/application.h>
 
 Glib::RefPtr<Gtk::Application> app;
-config_hud config_main;
+class syshud {};
 syshud* win;
 
-typedef syshud* (*syshud_create_func)(const config_hud &cfg);
+typedef syshud* (*syshud_create_func)(const std::map<std::string, std::map<std::string, std::string>>&);
 syshud_create_func syshud_create_ptr;
--- src/window.cpp
+++ src/window.cpp
@@ -5,18 +5,16 @@
 #include <filesystem>
 #include <thread>
 
-syshud::syshud(const config_hud &cfg) {
-	config_main = cfg;
-
+syshud::syshud(const std::map<std::string, std::map<std::string, std::string>>& cfg) : config_main(cfg) {
 	// Initialize layer shell
 	gtk_layer_init_for_window(gobj());
 	gtk_layer_set_namespace(gobj(), "syshud");
 	gtk_layer_set_layer(gobj(), GTK_LAYER_SHELL_LAYER_OVERLAY);
 
-	bool edge_top = (config_main.position.find("top") != std::string::npos);
-	bool edge_right = (config_main.position.find("right") != std::string::npos);
-	bool edge_bottom = (config_main.position.find("bottom") != std::string::npos);
-	bool edge_left = (config_main.position.find("left") != std::string::npos);
+	bool edge_top = (config_main["main"]["position"].find("top") != std::string::npos);
+	bool edge_right = (config_main["main"]["position"].find("right") != std::string::npos);
+	bool edge_bottom = (config_main["main"]["position"].find("bottom") != std::string::npos);
+	bool edge_left = (config_main["main"]["position"].find("left") != std::string::npos);
 
 	gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_TOP, edge_top);
 	gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_RIGHT, edge_right);
@@ -41,19 +39,19 @@ syshud::syshud(const config_hud &cfg) {
 	std::thread(&syshud::setup_monitors, this).detach();
 
 	// Set layout
-	if (config_main.orientation == 'h') {
+	if (config_main["main"]["orientation"][0] == 'h') {
 		// Horizontal layout
 		get_style_context()->add_class("horizontal");
-		set_default_size(config_main.width, config_main.height);
+		set_default_size(std::stoi(config_main["main"]["width"]), std::stoi(config_main["main"]["height"]));
 
 		// Check to see if the icon should be shown
-		if (config_main.icon_size != 0)
+		if (std::stoi(config_main["main"]["icon-size"]) != 0)
 			box_layout.append(image_volume);
 
 		box_layout.append(scale_volume);
 
 		// Check to see if the percentage should be shown
-		if (config_main.show_percentage)
+		if (config_main["main"]["show-percentage"] == "true")
 			box_layout.append(label_volume);
 
 		scale_volume.set_hexpand(true);
@@ -69,21 +67,21 @@ syshud::syshud(const config_hud &cfg) {
 			revealer_box.set_valign(Gtk::Align::END);
 		}
 	}
-	else if (config_main.orientation == 'v') {
+	else if (config_main["main"]["orientation"][0] == 'v') {
 		// Vertical layout
 		get_style_context()->add_class("vertical");
 		scale_volume.set_orientation(Gtk::Orientation::VERTICAL);
 		box_layout.property_orientation().set_value(Gtk::Orientation::VERTICAL);
-		set_default_size(config_main.height, config_main.width);
+		set_default_size(std::stoi(config_main["main"]["width"]), std::stoi(config_main["main"]["height"]));
 
 		// Check to see if the percentage should be shown
-		if (config_main.show_percentage)
+		if (config_main["main"]["show-percentage"] == "true")
 			box_layout.append(label_volume);
 
 		box_layout.append(scale_volume);
 
 		// Check to see if the icon should be shown
-		if (config_main.icon_size != 0)
+		if (std::stoi(config_main["main"]["icon-size"]) != 0)
 			box_layout.append(image_volume);
 
 		scale_volume.set_vexpand(true);
@@ -101,12 +99,12 @@ syshud::syshud(const config_hud &cfg) {
 		}
 	}
 	else {
-		std::fprintf(stderr, "Unknown orientation: %d\n", config_main.orientation);
+		std::fprintf(stderr, "Unknown orientation: %s\n", config_main["main"]["orientation"].c_str());
 		return;
 	}
 
 	// Set margins
-	std::istringstream iss(config_main.margins);
+	std::istringstream iss(config_main["main"]["margins"]);
 	std::string margin_str;
 	int count = 0;
 
@@ -125,7 +123,7 @@ syshud::syshud(const config_hud &cfg) {
 	}
 
 	// Animations disabled
-	if (config_main.transition_time == 0)
+	if (std::stoi(config_main["main"]["transition-time"]) == 0)
 		set_child(box_layout);
 
 	// Animations enabled
@@ -133,7 +131,7 @@ syshud::syshud(const config_hud &cfg) {
 		set_child(revealer_box);
 		revealer_box.set_child(box_layout);
 		revealer_box.set_transition_type(transition_type);
-		revealer_box.set_transition_duration(config_main.transition_time);
+		revealer_box.set_transition_duration(std::stoi(config_main["main"]["transition-time"]));
 		revealer_box.set_reveal_child(false);
 	}
 
@@ -142,13 +140,13 @@ syshud::syshud(const config_hud &cfg) {
 
 	scale_volume.signal_change_value().connect(sigc::mem_fun(*this, &syshud::on_scale_change), true);
 
-	if (config_main.icon_size != 0) {
-		image_volume.set_pixel_size(config_main.icon_size);
-		image_volume.set_size_request(config_main.height, config_main.height);
+	if (std::stoi(config_main["main"]["icon-size"]) != 0) {
+		image_volume.set_pixel_size(std::stoi(config_main["main"]["icon-size"]));
+		image_volume.set_size_request(std::stoi(config_main["main"]["height"]), std::stoi(config_main["main"]["height"]));
 	}
 
-	if (config_main.show_percentage)
-		label_volume.set_size_request(config_main.height, config_main.height);
+	if (config_main["main"]["show-percentage"] == "true")
+		label_volume.set_size_request(std::stoi(config_main["main"]["height"]), std::stoi(config_main["main"]["height"]));
 
 	dispatcher_audio_in.connect(sigc::bind(sigc::mem_fun(*this, &syshud::on_audio_callback), true));
 	dispatcher_audio_out.connect(sigc::bind(sigc::mem_fun(*this, &syshud::on_audio_callback), false));
@@ -180,19 +178,19 @@ void syshud::on_change(const char &reason, const int &value) {
 	last_reason = reason;
 
 	if (timer_ticking)
-		timeout = config_main.desired_timeout;
+		timeout = std::stoi(config_main["main"]["timeout"]);
 
 	else if (timeout == 1) {
 		show();
 
 		revealer_box.set_reveal_child(true);
 		timer_ticking = true;
-		timeout = config_main.desired_timeout;
+		timeout = std::stoi(config_main["main"]["timeout"]);
 		Glib::signal_timeout().connect(sigc::mem_fun(*this, &syshud::timer), 1000);
 	}
 
 	// Check if we should draw the icons or not
-	if (config_main.icon_size == 0)
+	if (std::stoi(config_main["main"]["icon-size"]) == 0)
 		return;
 
 	// Map
@@ -257,7 +255,7 @@ void syshud::on_change(const char &reason, const int &value) {
 	scale_volume.set_value(value);
 
 	// Check if we should draw the percentage
-	if (config_main.show_percentage)
+	if (config_main["main"]["show-percentage"] == "true")
 		label_volume.set_label(label);
 }
 
@@ -310,7 +308,7 @@ void syshud::on_keytoggle_callback() {
 }
 
 void syshud::setup_monitors() {
-	std::istringstream iss(config_main.monitors);
+	std::istringstream iss(config_main["main"]["monitors"]);
 	std::string monitor;
 
 	Glib::Dispatcher *audio_in = nullptr;
@@ -327,10 +325,10 @@ void syshud::setup_monitors() {
 			audio_out = &dispatcher_audio_out;
 		}
 		else if (monitor == "brightness") {
-			backlight = new syshud_backlight(&dispatcher_backlight, config_main.backlight_path);
+			backlight = new syshud_backlight(&dispatcher_backlight, config_main["main"]["backlight-path"]);
 		}
-		else if (monitor == "keyboard" && config_main.keyboard != "" && config_main.orientation == 'h') {
-			keytoggle_watcher = new syshud_keytoggles(&dispatcher_keytoggles, config_main.keyboard);
+		else if (monitor == "keyboard" && config_main["main"]["keyboard-path"] != "" && config_main["main"]["orientation"][0] == 'h') {
+			keytoggle_watcher = new syshud_keytoggles(&dispatcher_keytoggles, config_main["main"]["keyboard-path"]);
 		}
 		else {
 			std::fprintf(stderr, "Unknown monitor: %s\n", monitor.c_str());
@@ -358,7 +356,7 @@ bool syshud::timer() {
 		timeout_connection = Glib::signal_timeout().connect([&]() {
 			hide();
 			return false;
-		}, config_main.transition_time);
+		}, std::stoi(config_main["main"]["transition-time"]));
 
 		return false;
 	}
@@ -368,7 +366,7 @@ bool syshud::timer() {
 }
 
 extern "C" {
-	syshud *syshud_create(const config_hud &cfg) {
+	syshud *syshud_create(const std::map<std::string, std::map<std::string, std::string>>& cfg) {
 		return new syshud(cfg);
 	}
 }
