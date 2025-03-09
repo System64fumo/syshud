8ada9d2a6802d4810751f060f849a8e4520e58bd
system64fumo <system64fumo@protonmail.com>
2025-03-09 09:40:17 +0200

    Fix critical keyboard bug
    Fixed 100% CPU usage caused by keyboard disconnect.
    Not really happy with this solution and i'm scared of trying the alternative from my file manager..

--- src/keytoggles.cpp
+++ src/keytoggles.cpp
@@ -7,59 +7,69 @@
 #include <libevdev/libevdev.h>
 
 syshud_keytoggles::syshud_keytoggles(Glib::Dispatcher* callback, const std::string& device_path) {
-	struct libevdev* dev = nullptr;
-	int fd = open(device_path.c_str() , O_RDONLY | O_NONBLOCK);
+	std::thread([&, device_path, callback]() {
+		struct libevdev* dev = nullptr;
+		int fd = -1;
 
-	if (fd < 0) {
-		std::fprintf(stderr, "Failed to open device\n");
-		return;
-	}
-
-	if (libevdev_new_from_fd(fd, &dev) < 0) {
-		close(fd);
-		return;
-	}
-
-	// Get initial states
-	caps_lock_prev = libevdev_get_event_value(dev, EV_LED, LED_CAPSL);
-	num_lock_prev = libevdev_get_event_value(dev, EV_LED, LED_NUML);
-
-	std::thread([&, fd, dev, callback]() {
 		while (true) {
-			struct input_event ev;
-			int rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
-
-			// TODO: THIS IS TERRIBLE!!!!
-			// This wastes a lot of battery and CPU power
-			// *But it's a good temporary solution for now*
-			if (rc == -EAGAIN) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			// Attempt to open the device
+			fd = open(device_path.c_str(), O_RDONLY | O_NONBLOCK);
+			if (fd < 0) {
+				std::this_thread::sleep_for(std::chrono::seconds(1));
 				continue;
 			}
 
-			// Process LED events (Caps Lock, Num Lock, Scroll Lock)
-			if (ev.type != EV_LED)
+			// Attempt to initialize libevdev
+			if (libevdev_new_from_fd(fd, &dev) < 0) {
+				close(fd);
+				std::this_thread::sleep_for(std::chrono::seconds(1));
 				continue;
+			}
 
-			// What the hell is this mess???
-			if (ev.code == LED_CAPSL)
-				caps_lock = ev.value;
+			// Get initial states
+			caps_lock_prev = libevdev_get_event_value(dev, EV_LED, LED_CAPSL);
+			num_lock_prev = libevdev_get_event_value(dev, EV_LED, LED_NUML);
 
-			else if (ev.code == LED_NUML)
-				num_lock = ev.value;
+			// Main event loop
+			while (true) {
+				struct input_event ev;
+				int rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
 
-			if (caps_lock != caps_lock_prev)
-				changed = 'c';
+				if (rc == -EAGAIN) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(100));
+					continue;
+				}
 
-			else if (num_lock != num_lock_prev)
-				changed = 'n';
+				// Check if the device is still connected
+				if (rc == -ENODEV)
+					break;
 
-			caps_lock_prev = caps_lock;
-			num_lock_prev = num_lock;
+				// Process LED events (Caps Lock, Num Lock, Scroll Lock)
+				if (ev.type != EV_LED)
+					continue;
 
-			callback->emit();
+				if (ev.code == LED_CAPSL)
+					caps_lock = ev.value;
+
+				else if (ev.code == LED_NUML)
+					num_lock = ev.value;
+
+				if (caps_lock != caps_lock_prev)
+					changed = 'c';
+
+				else if (num_lock != num_lock_prev)
+					changed = 'n';
+
+				caps_lock_prev = caps_lock;
+				num_lock_prev = num_lock;
+
+				callback->emit();
+			}
+
+			libevdev_free(dev);
+			close(fd);
+			dev = nullptr;
+			fd = -1;
 		}
-		libevdev_free(dev);
-		close(fd);
 	}).detach();
 }
